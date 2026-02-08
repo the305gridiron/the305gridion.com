5bedfdb940b3a37f8f77995bfd107880c628c2cc
Anthony Pinzone <the1andonlypz@gmail.com>
2026-02-08 12:39:20 -0500

    Added Layout component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,29 @@
 import { Outlet, Link } from "react-router-dom";
+import Logo from "../assets/the-305-gridiron-logo.jpeg";
+import styles from "./Layout.module.css";
+import { ArrowLeftRight, ListOrdered, PlaySquare, Video } from "lucide-react";
 
 export default function Layout() {
     return (
-        <>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/draft">2026 Big Board</Link>
-                <Link to="/offseason">Offseason Tracker</Link>
-            </nav>
+        <main className={styles.pageLayout}>
+            <header className={styles.pageHeader}>
+                <div className={`container-fluid ${styles.pageHeaderContainer}`}>
+                    <figure className={styles.logo}>
+                        <img src={Logo} alt="The 305 Gridiron Logo" />
+                    </figure>
+                    <nav className={styles.pageNav}>
+                        <Link to="/" className={styles.navLink}><Video size={18} />
+                            Video Library</Link>
+                        <Link to="/draft" className={styles.navLink}><ListOrdered size={18} /> Draft Board</Link>
+                        <Link to="/offseason" className={styles.navLink}><ArrowLeftRight size={18} /> Offseason Tracker</Link>
+                    </nav>
+                    <button className={styles.subscribeBtn} onClick={() => window.open("https://www.youtube.com/channel/UC2FolYfTCRIBP1s3ckiPb_w?sub_confirmation=1", "_blank")}>
+                        <PlaySquare size={18} /> Subscribe
+                    </button>
+                </div>
+            </header>
 
             <Outlet />
-        </>
+        </main>
     );
 }
