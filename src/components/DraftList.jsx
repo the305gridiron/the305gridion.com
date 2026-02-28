0b24c12c0db880a78ba3046312663241807dcd81
AnthonyPinzone <the1andonlypz@gmail.com>
2026-02-28 17:30:53 -0500

    Added DraftList component

---
+++ src/components/DraftList.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import DraftCard from "./DraftCard";
+import TrendingUpOutlinedIcon from "@mui/icons-material/TrendingUpOutlined";
+
+import styles from "./DraftList.module.css";
+
+export default function DraftList(props) {
+    return (
+        <div id='draftList' className={styles.draftList}>
+            <div className={styles.draftCards}>
+                {props.players && props.players.length > 0 ? (
+                    props.players.map((player) => (
+                        <DraftCard key={player.id} {...player} />
+                    ))
+                ) : (
+                    <p>No one drafted yet!</p>
+                )}
+            </div>
+        </div>
+    );
+}
