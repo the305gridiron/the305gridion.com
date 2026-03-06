ba99f5c7266cff9e238e98fd87221422040b5ba7
Anthony Pinzone <the1andonlypz@gmail.com>
2026-03-05 19:50:35 -0500

    Layout adjustments.

--- src/layout/Hero.jsx
+++ src/layout/Hero.jsx
@@ -1,9 +1,28 @@
 import styles from "./Hero.module.css";
 
-export default function Hero({ children }) {
+function Hero({ children, ...props }) {
     return (
         <div className={styles.hero}>
             <div className='container'>{children}</div>
         </div>
     );
 }
+
+function Title({ children, className = "" }) {
+    return (
+        <h1 className={`${styles.title} ${className}`}>
+            {children}
+        </h1>
+    );
+}
+
+function Promo({ children, className = "" }) {
+    return (
+        <p className={`${styles.promo} ${className}`}>{children}</p>
+    );
+}
+
+Hero.Title = Title;
+Hero.Promo = Promo;
+
+export default Hero;
