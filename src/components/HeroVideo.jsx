db0b9a0e55577b0337f5806fca74f06f276ac308
AnthonyPinzone <the1andonlypz@gmail.com>
2026-03-10 19:36:38 -0400

    Fixed livestream

--- src/components/HeroVideo.jsx
+++ src/components/HeroVideo.jsx
@@ -8,25 +8,49 @@ export default function HeroVideo({ video }) {
 
     return (
         <section className={styles.heroVideo}>
-            <div className="container">
+            <div className='container'>
                 <div className={styles.videoWrapper}>
                     <YoutubeEmbed
                         videoId={video.videoId}
                         thumbnail={video.thumbnail}
-                        title={video.title}
+                        title={video.title.replace("&amp;", "&")}
                         thumbnailStyles={{ border: 0 }}
                         autoplay={video.isLive}
                     />
                 </div>
                 <div className={styles.videoDetails}>
-                    <h2 className={styles.title}>{video.title.replace("|", "")}</h2>
-                    <p className={styles.description}>{video.description.split("📢")[0]}</p>
+                    <h2 className={styles.title}>
+                        {video.title.replace("|", "").replace("&amp;", "&")}
+                    </h2>
+                    <p className={styles.description}>
+                        {video?.description?.split("📢")[0]}
+                    </p>
                     <div className={styles.heroCTA}>
-                        <a className={styles.heroButton} href="https://www.youtube.com/playlist?list=PLxSEosyZR98Psz7lXdABd7gJ7JynnIkYm" target="_blank" rel="noopener noreferrer">
-                            <span className={styles.buttonIcon}><GraduationCap /></span> Checkout Our<br />2025 Position Grades
+                        <a
+                            className={styles.heroButton}
+                            href='https://www.youtube.com/playlist?list=PLxSEosyZR98Psz7lXdABd7gJ7JynnIkYm'
+                            target='_blank'
+                            rel='noopener noreferrer'
+                        >
+                            <span className={styles.buttonIcon}>
+                                <GraduationCap />
+                            </span>{" "}
+                            Checkout Our
+                            <br />
+                            2025 Position Grades
                         </a>
-                        <a className={styles.heroButton} href="https://www.youtube.com/playlist?list=PLxSEosyZR98OToPtZ4JcSFPkqGbGc3Q6u" target="_blank" rel="noopener noreferrer">
-                            <span className={styles.buttonIcon}><GitPullRequestArrow /></span> Get More 2026<br />Offseason Content
+                        <a
+                            className={styles.heroButton}
+                            href='https://www.youtube.com/playlist?list=PLxSEosyZR98OToPtZ4JcSFPkqGbGc3Q6u'
+                            target='_blank'
+                            rel='noopener noreferrer'
+                        >
+                            <span className={styles.buttonIcon}>
+                                <GitPullRequestArrow />
+                            </span>{" "}
+                            Get More 2026
+                            <br />
+                            Offseason Content
                         </a>
                     </div>
                 </div>
