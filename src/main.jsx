bc4398eb6831e7a645b54b5a74d837dd62b48bc1
Anthony Pinzone <the1andonlypz@gmail.com>
2026-04-08 14:59:24 -0400

    Updated tanstack storage persister to latest version.

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { persistQueryClient } from "@tanstack/react-query-persist-client";
-import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App.jsx";
 
@@ -15,7 +15,7 @@ const queryClient = new QueryClient({
     },
 });
 
-const persister = createSyncStoragePersister({
+const persister = createAsyncStoragePersister({
     storage: window.localStorage,
 });
 
